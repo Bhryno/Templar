b7932e05f4db607c617d64dd4c539424d339b9d2
HairConditioner <bryanrplee@gmail.com>
2023-11-12 11:20:40 +1100

    Basic note page framework

--- src/app/note/[noteId]/page.tsx
+++ src/app/note/[noteId]/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+import { clerk } from '@/lib/clerk-server'
 import { db } from '@/lib/db'
 import { $notes } from '@/lib/db/schema'
 import { auth } from '@clerk/nextjs'
 import { and, eq } from 'drizzle-orm'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
@@ -11,25 +14,51 @@ type Props = {
 	}
 }
 
-const NotebookPage = async ({ params: { noteId } }: Props) => {
+const Note = async ({ params: { noteId } }: Props) => {
 	const { userId } = await auth()
 
-    if (!userId) {
-        return redirect('/dashboard')
-    }
+	if (!userId) {
+		return redirect('/dashboard')
+	}
 
+	const user = clerk.users.getUser(userId)
 	const notes = await db
 		.select()
 		.from($notes)
 		.where(and(eq($notes.id, parseInt(noteId)), eq($notes.userId, userId)))
 
-    if (notes.length != 1) {
-            return redirect('/dashboard')
-    }
+	if (notes.length != 1) {
+		return redirect('/dashboard')
+	}
 
-    const note = notes[0]
+	const note = notes[0]
 
-	return <pre>{JSON.stringify(note, null, 2)}</pre>
+	return (
+		<div className="min-h-screen grainy p-8">
+			<div className="max-w-4xl mx-auto">
+				<div className="border shadow-xl border-stone-200 rounded-lg p-4 flex items-center">
+					<Link href="/dashboard">
+						<Button className="bg-yellow-300" size="sm">
+							Back
+						</Button>
+					</Link>
+					<div className="w-3"></div>
+					<span className="font-semibold">
+						{(await user).firstName} {(await user).lastName}
+					</span>
+					<span className="inline-block mx-1">/</span>
+					<span className="text-stone-500 font-semibold">
+						{note.name}
+					</span>
+					<div className="ml-auto">DELETE BUTTON</div>
+				</div>
+				<div className="h-4"></div>
+				<div className="border-stone-200 shadow-xl border-rounded-lg px-16 py-8 w-full">
+					{/* Editor */}
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default NotebookPage
+export default Note

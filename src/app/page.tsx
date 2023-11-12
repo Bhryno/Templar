e191b20f5c2e3e127dd806eb0ec80172df9d8316
HairConditioner <bryanrplee@gmail.com>
2023-11-12 15:51:38 +1100

    Homepage restyle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 		<div className="min-h-screen grainy">
 			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 				<h1 className="font-semibold text-7xl text-center">
-					AI{' '}
+					A powerful{' '}
 					<span className="text-yellow-300 font-bold">
 						note-taking
 					</span>{' '}

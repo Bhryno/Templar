cd4d7be10ac3c36a136669db30b549fc34a8f04a
HairConditioner <bryanrplee@gmail.com>
2023-11-12 11:10:15 +1100

    User 'Note' page backend

--- src/app/note/[noteId]/page.tsx
+++ src/app/note/[noteId]/page.tsx
@@ -1,3 +1,8 @@
+import { db } from '@/lib/db'
+import { $notes } from '@/lib/db/schema'
+import { auth } from '@clerk/nextjs'
+import { and, eq } from 'drizzle-orm'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 type Props = {
@@ -6,8 +11,25 @@ type Props = {
 	}
 }
 
-const NotebookPage = (props: Props) => {
-	return <div>NotebookPage</div>
+const NotebookPage = async ({ params: { noteId } }: Props) => {
+	const { userId } = await auth()
+
+    if (!userId) {
+        return redirect('/dashboard')
+    }
+
+	const notes = await db
+		.select()
+		.from($notes)
+		.where(and(eq($notes.id, parseInt(noteId)), eq($notes.userId, userId)))
+
+    if (notes.length != 1) {
+            return redirect('/dashboard')
+    }
+
+    const note = notes[0]
+
+	return <pre>{JSON.stringify(note, null, 2)}</pre>
 }
 
 export default NotebookPage
